ebe86f06237989d3a676029e193340022c6b6c7f
MoovendhanR <moovendhanr1011@gmail.com>
2023-04-19 23:57:50 +0530

    database created

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const mongoose=require('mongoose');
+const connect = require('./config/db.js');
+const cors=require("cors");
 
 const app=express();
+app.use(cors({
+    origin:"*"
+}))
+
+app.use(express.json());
 
 
 
-
-
-app.listen(5000,()=>{
+mongoose.set("strictQuery",true);
+app.listen(5000,async()=>{
+    await connect();
     console.log('listening on port 5000')
 })
